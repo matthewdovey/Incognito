d7c7e1f5960b34a8148509649dead41447472c85
matthewdovey <matthewdovey@hotmail.co.uk>
2016-12-11 10:44:52 +0000

    Console Development

--- Incognito/src/Client/Console.java
+++ Incognito/src/Client/Console.java
@@ -1,59 +1,121 @@
-import javafx.event.ActionEvent;
+package sample;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 
 import java.util.ArrayList;
 
-/**
- * Created by mdovey on 07/12/2016.
- */
-public class Console {
+public class Console extends BorderPane{
     private TextField consoleInput;
     private TextArea consoleOutput;
     private ArrayList<String> consoleHistory;
+    private ArrayList<String> displayHistory;
+    private ArrayList<String> commands;
+    private int index = 0;
 
     public Console() {
+        getCommands();
         consoleInput = new TextField();
         consoleOutput = new TextArea();
-        consoleHistory = new ArrayList<String>();
+        consoleHistory = new ArrayList<>();
+        displayHistory = new ArrayList<>();
 
+        consoleInput.setStyle("-fx-background-color: #000000;" + "-fx-text-inner-color: white;");;
         consoleOutput.setEditable(false);
+        setCenter(consoleOutput);
+        setBottom(consoleInput);
 
-        consoleInput.addEventHandler(KeyEvent.KEY_RELEASED, event -> {
-            if (event.getCode() == KeyCode.ENTER) {
-                consoleInputHandler(consoleInput.getText());
+        consoleInput.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()) {
+                case ENTER:
+                    consoleInputHandler(consoleInput.getText());
+                    index = 0;
+                    consoleInput.clear();
+                    break;
+                case UP:
+                    if (!consoleHistory.isEmpty()) {
+                        if (index == consoleHistory.size()) {
+                            index = 0;
+                        }
+                        consoleInput.setText(consoleHistory.get(index));
+                        index++;
+                    }
+                    break;
+                case DOWN:
+                    if (!consoleHistory.isEmpty() && index > 0) {
+                        consoleInput.setText((consoleHistory.get(index-1)));
+                        index--;
+                    }
             }
         });
     }
 
     private void consoleInputHandler(String command) {
         command = command.toLowerCase();
-        if (inputCheck(command)) {
-            consoleHistory.add(command);
-            if (command == "help") {
-                help();
+
+        if (isCommand(command)) {
+            consoleHistory.add(0,command);
+            displayHistory.add(command);
+            updateOutput();
+            switch (command) {
+                case "help":
+                    System.out.println("help");
+                    help();
+                    break;
+                case "scan":
+                    break;
+                case "clear":
+                    consoleOutput.clear();
+                    displayHistory.clear();
+                    break;
             }
+        } else {
+            displayErrorMessage();
         }
     }
 
-    private boolean inputCheck(String command) {
-        if (command == null) {
-            return false;
+    private boolean isCommand(String command) {
+        return commands.contains(command);
+    }
+
+    private void getCommands() {
+        commands = new ArrayList<>();
+        commands.add("help");
+        commands.add("scan");
+        commands.add("ping");
+        commands.add("clear");
+    }
+
+    private void updateOutput() {
+        String output = "";
+        for (String command : displayHistory) {
+            output += command + "\n";
         }
-        //Do I want to allow users to enter blank lines??? to clean up the console a bit
-        if (command.length() > 0) {
-            return false;
-        }
-        return true;
+        consoleOutput.setText(output);
+        consoleOutput.setScrollTop(Double.MAX_VALUE);
     }
 
     private void help() {
         System.out.println("help");
-        //check the diferences between the two
-        consoleOutput.insertText(1,"help");
-        consoleOutput.setText("help");
+        String output = "";
+        for (String command : displayHistory) {
+            output += command + "\n";
+        }
+        output += "help - display all valid commands\nclear - clears command history";
+        consoleOutput.setText(output);
+        consoleOutput.setScrollTop(Double.MAX_VALUE);
+    }
+
+    private void displayErrorMessage() {
+        String output = "";
+        for (String command : displayHistory) {
+            output += command + "\n";
+        }
+        output += "Invalid command entered...";
+        displayHistory.add("Invalid command entered...");
+        consoleOutput.setText(output);
+        consoleOutput.setScrollTop(Double.MAX_VALUE);
     }
 
 }
